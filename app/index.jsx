5b0be6669606b0b67b392a96611198a7d0be3a2c
Shashwat Ashiya <129604780+shashwatashiya@users.noreply.github.com>
2024-05-06 08:23:46 +0530

    setup

---
+++ app/index.jsx
@@ -0,0 +1,16 @@
+import {Text, View } from 'react-native'
+import React from 'react'
+
+const rootLayout = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-2xl">Welcome to Cattle Market!</Text>
+    </View>
+  )
+}
+
+export default rootLayout
+
+
+//this is our home page
+
